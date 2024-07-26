20604efb1f9c91c970d9db90675e659ec5b096b8
baejunghwan <hwanarchive@gmail.com>
2024-07-26 09:02:59 +0900

    123

---
+++ src/com/mycompany/ch01/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.mycompany.ch01;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		// T000 Auto-generated method stub
+		
+		System.out.println("Welcome to hello world !!!");
+	}
+	
+}
+
+d
